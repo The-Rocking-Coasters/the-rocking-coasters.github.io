7521d6d898f15cc766f9a01bd8c83d5ae8d4c719
Rick Pennings <rick@intractief.nl>
2026-03-12 19:22:21 +0100

    slug fixed

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -4,34 +4,16 @@ const route = useRoute()
 const { locale } = useI18n()
 
 const { data: page } = await useAsyncData(route.path, () => {
-  // If we are on root, use default locale 'nl'
-  // If the route is /en, query a collection with path /en/
-  // Use locales to determine if it's a locale prefix
+  const { locale } = useI18n()
   let path = route.path
-  if (path === '/' || path === '/en' || path === '/en/') {
-    path = `/${locale.value}/index`
-  } else if (path.endsWith('/')) {
-    path = `${path}index`
-  } else if (!path.endsWith('/index')) {
-    path = `${path}/index`
-  }
-  
-  // If strategy is prefix_except_default, 'nl' routes won't have /nl prefix in route.path
-  // But content is in /nl/ directory.
-  if (locale.value === 'nl' && !path.startsWith('/nl/')) {
+
+  if (locale.value === 'nl' && !path.startsWith('/nl')) {
     path = `/nl${path}`
   }
 
-  // Ensure we don't have double slashes
-  path = path.replace(/\/+/g, '/')
-  
-  // Strip trailing /index if it's there, as it seems Nuxt Content v3 prefers the directory path
-  if (path.endsWith('/index')) {
-    path = path.substring(0, path.length - 6)
-  }
+  path = path.replace(/\/+/g, '/').replace(/\/$/, '')
   if (path === '') path = '/'
 
-  // Ensure a path ends with a slash for a directory query if needed, or just a path
   return queryCollection('content').path(path).first()
 })
 
